c0d0606982cc92f888e1551f3530b46e0b61126d
nitunwa <nitun.2@gmail.com>
2020-04-22 21:36:22 -0700

    show title based on location

--- src/main.js
+++ src/main.js
@@ -15,17 +15,17 @@ $(document).ready(function() {
       getResponse(response);
     })();
 
-   
+    //$("<li></li>").html(demoTask).appendTo(list);
 
     function getResponse(response) {
       //let bikeArray = [];
       if (response) {
         let count=0;
         for(let i=0;i<response.bikes.length;i++){
-           console.log("city1   " + response.bikes[i].stolen_location )
           if(response.bikes[i].stolen_location === city){
-            console.log("city1   " + response.bikes[i].stolen_location )
             count++;
+            let list = $("#showList");
+            $("<li></li>").html(response.bikes[i].title).appendTo(list);
           }
         }
         $('#showBikeCount').text(`You are searching in ${city} and there are ${count} stolen bikes.`);
